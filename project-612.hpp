7a569a3f569df819926d81ea02e6b205814a8dc1
Alex20129 <smokevsop@gmail.com>
2021-02-18 21:53:31 +0300

    PROJECT_612_HPP

--- project-612.hpp
+++ project-612.hpp
@@ -1,8 +1,8 @@
-#ifndef FANN_TEST_APP_H
-#define FANN_TEST_APP_H
+#ifndef PROJECT_612_HPP
+#define PROJECT_612_HPP
 
 //#include <iostream>
 #include <stdio.h>
 #include <unistd.h>
 
-#endif // FANN_TEST_APP_H
+#endif // PROJECT_612_HPP
